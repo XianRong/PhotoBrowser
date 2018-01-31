f31541020bd9635ac9b10a02a8ae084fea0828e2
XianRong <yhtyhrt6@qq.com>
2018-01-31 09:57:38 +0800

    增加一个宏文件测试

---
+++ Macro.swift
@@ -0,0 +1,20 @@
+//
+//  Macro.swift
+//  freetime
+//
+//  Created by 贤荣 on 2017/8/30.
+//  Copyright © 2017年 xianrong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let kScreenW = UIScreen.main.bounds.width
+let kScreenH = UIScreen.main.bounds.height
+
+let kNavigationBarH : CGFloat = 44
+let kStatusBarH = UIApplication.shared.statusBarFrame.height
+let kNavigationH :CGFloat = kNavigationBarH + kStatusBarH
+
+let kBannarH = kScreenW/1.62 // 轮播图黄金比例
+
